ee8b5b2ce0bd9ca852a726cd3f014d89ec9d2101
太雅 <idegreat555@gmail.com>
2020-07-19 13:00:52 +0000

    PHP/Laravel 03 課題完了

--- practice2.php
+++ practice2.php
@@ -1,27 +1,21 @@
 <?php
 
 #課題1
-/*$name = "井出太雅";
+$name = "井出太雅";
 if ($name = "井出太雅") {
  echo "私は あなたの名前 です";
 } else {
  echo "あなたの名前ではありません";
-}*/
+}
 
 #課題2
-/*for ($i = 0; $i <= 10000; $i++){
+for ($i = 0; $i <= 10000; $i++){
     $total += $i;
-}
-echo $total;*/
-
-for ($i = 0; $i <= 100; $i++) {
-  $total += $i;
 }
 echo $total;
 
-
 #課題3
-/*$fruits = array("grape","orange","apple","grapefruit","acerola");
+$fruits = array("grape","orange","apple","grapefruit","acerola");
 foreach($fruits as $fruit){
     echo $fruit;
     echo "\n";
